b0b9e02a4e727d46b4ac463812612887309bcd8a
prabu <prabuarjunan@gmail.com>
2020-07-30 16:11:32 -0700

    change update volume

--- TimerTrigger/__init__.py
+++ TimerTrigger/__init__.py
@@ -6,8 +6,7 @@ import azure.functions as func
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
-    volumeShapingsizing.update_volume
-
+    volumeShapingsizing.update_volume()
     if mytimer.past_due:
         logging.info('The timer is past due!')
 
